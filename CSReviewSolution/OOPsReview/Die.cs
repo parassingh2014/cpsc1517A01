1a597b6cc5796f467519fb49ad852af2edbdcb45
parassingh2014 <43007109+parassingh2014@users.noreply.github.com>
2019-01-23 14:28:08 -0700

    first razor

---
+++ CSReviewSolution/OOPsReview/Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Die
+    {
+        //Data Members
+        //this is the physical storage area for data values
+        //these are usually private
+
+        private int _Sides;
+        private string _Color;
+        private int _FaceValue;
+
+        //Properties
+        //Properties are public
+        // is used as an interface for the class user to access
+        //  a data member
+        // acccessing a data member can include a get and a set
+        // a get returns the value of the data member to the user
+        // a set accepts a value and assigns the value 
+        // to the dat member
+        // a property return a specific datatype
+
+        //Fully implemented property
+        public int Side
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Die.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Die
+    {
+        private String _color;
+
+        public String Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (_color == " ")
+                    throw new Exception("Cannot be empty.");
+                else
+                    _color = value;
+            }
+        }
+    }
+}
